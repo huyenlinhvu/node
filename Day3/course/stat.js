a1187a0e24cc891cb17ae696af10c39aeac1a913
Linh <huyenlinhvu.pro@gmail.com>
2023-04-05 14:23:29 +0200

    course server

---
+++ Day3/course/stat.js
@@ -0,0 +1,15 @@
+const fs = require('fs'),
+    http = require('http');
+
+    http.createServer(function(req, res) {
+        fs.readFile(__dirname + req.url, (err, data) => {
+            if(err) {
+                res.writeHead(404);
+                res.end(JSON.stringify(err));
+                return;
+            }
+
+            res.writeHead(200);
+            res.end(data);
+        })
+    }).listen(8000);
