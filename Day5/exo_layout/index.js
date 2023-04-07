b538f37f73892c173137b43892e38e21b098dce5
Linh <huyenlinhvu.pro@gmail.com>
2023-04-07 11:19:10 +0200

    exo layout

---
+++ Day5/exo_layout/index.js
@@ -0,0 +1,6 @@
+const menuItems = [
+    { path: '/', title: 'Home', isActive: true },
+    { path: '/about-me', title: 'About', isActive: false },
+    { path: '/references', title: 'References', isActive: false },
+    { path: '/contact-me', title: 'Contact', isActive: false },
+];
