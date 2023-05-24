fc5ace4ec9698876c6fe86af188c1c68f51a77c8
Linh <huyenlinhvu.pro@gmail.com>
2023-05-24 17:33:15 +0200

    add filter

--- routers/passengers.js
+++ routers/passengers.js
@@ -9,8 +9,31 @@ appRouter.get("/", (req, res) => {
 
 appRouter.get("/list", async (req, res) => {
   try {
-    const passengers = await Passenger.find();
-    console.log(typeof passengers);
+    let filter = {}; // Initialize an empty filter object
+
+    // Retrieve the filter values from the query parameters
+    const { gender, ticketClass, age } = req.query;
+
+    // Add the filter conditions based on the selected criteria
+    if (gender && gender !== "all") {
+      filter.Sex = gender;
+    }
+
+    if (ticketClass && ticketClass !== "all") {
+      filter.Pclass = ticketClass;
+    }
+
+    if (age && age !== "") {
+      if (age === "0-20") {
+        filter.Age = { $lte: 20 };
+      } else if (age === "21-50") {
+        filter.Age = { $gte: 21, $lte: 50 };
+      } else if (age === "50+") {
+        filter.Age = { $gte: 50 };
+      }
+    }
+
+    const passengers = await Passenger.find(filter);
     res.render("listPassengers", { passengers, messages: [] }); // Pass empty array as default value for messages
   } catch (error) {
     res.status(500).send("Error retrieving passengers");
